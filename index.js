29f48e1d538b60981737fdf79de3443e169a13e8
gunwant07 <144123267+gunwant07@users.noreply.github.com>
2023-10-04 16:46:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 3000
+var cors = require('cors');
+server.use(express.json());
+
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('connected');
+}
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('user', userSchema);
+
+server.post('/demo', async (req, res) => {
+  let user = new User();
+  user.username = req.body.username;
+  user.password = req.body.password;
+  const doc = await user.save();
+  console.log(doc);
+  res.json(doc);
+});
+
+server.listen(port, () => {
+ console.log(`Server listening on port ${port}`);
+  console.log('server started');
+});
